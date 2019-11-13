dbcfc82f1a081d9f14c39717b0b07c5115b47fb0
ductri <ductricse@gmail.com>
2019-11-13 15:29:13 +0700

    ok

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -6,6 +6,6 @@ source docker/config.txt
 
 docker logs -f --timestamps $(docker run --runtime=nvidia -d -e PYTHONIOENCODING=utf-8 --name="$PROJECT_NAME"_jupyter --rm \
 -v `pwd`/source/:/source \
--v `pwd`/../dataset:$DATASET_DIR:ro \
+-v $DATASET_DIR:/dataset:ro \
 -p $JUPYTER_PORT:$JUPYTER_PORT \
 ductricse/$BACKEND /bin/bash -c "jupyter notebook --port=$JUPYTER_PORT --allow-root --ip=0.0.0.0")
