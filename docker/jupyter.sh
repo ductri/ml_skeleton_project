ce69f906d89e566366701a6ff763dd896dd50e95
ductri <ductricse@gmail.com>
2019-11-13 15:28:14 +0700

    add dataset mapping

--- docker/jupyter.sh
+++ docker/jupyter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# PROJECT_NAME, JUPYTER_PORT, BACKEND
+# PROJECT_NAME, JUPYTER_PORT, BACKEND, DATASET_DIR
 
 source docker/config.txt
 
 docker logs -f --timestamps $(docker run --runtime=nvidia -d -e PYTHONIOENCODING=utf-8 --name="$PROJECT_NAME"_jupyter --rm \
 -v `pwd`/source/:/source \
--v `pwd`/../dataset:/dataset:ro \
+-v `pwd`/../dataset:$DATASET_DIR:ro \
 -p $JUPYTER_PORT:$JUPYTER_PORT \
 ductricse/$BACKEND /bin/bash -c "jupyter notebook --port=$JUPYTER_PORT --allow-root --ip=0.0.0.0")
